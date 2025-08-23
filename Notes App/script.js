37f6bd88dad832ac37b8672e7b355c965cb3fd75
AyushiAnand70047 <ayushianand088@gmail.com>
2025-08-23 09:57:24 +0530

    Notes App created

---
+++ Notes App/script.js
@@ -0,0 +1,34 @@
+const notesContainer = document.querySelector('.notes-container');
+const createBtn = document.querySelector('.create-btn');
+
+function showNotes(){
+    notesContainer.innerHTML = (localStorage.getItem("notes")).replace(/<div>(.*?)<\/div>/g, "$1<br>");
+}
+showNotes();
+
+function updateStorage(){
+    localStorage.setItem("notes",notesContainer.innerHTML);
+}
+
+createBtn.addEventListener('click', () => {
+    let p = document.createElement("p");
+    let img = document.createElement("img");
+    p.className = "input-box";
+    p.setAttribute("contenteditable","true");
+    img.src = "./images/delete.png";
+    notesContainer.appendChild(p).appendChild(img);
+})
+
+notesContainer.addEventListener("click",(e) => {
+    if(e.target.tagName == "IMG"){
+        e.target.parentElement.remove();
+        updateStorage();
+    } else if(e.target.tagName == "P"){
+        let notes = document.querySelectorAll('.input-box');
+        notes.forEach(note => {
+            note.onkeyup = function(){
+                updateStorage();
+            }
+        })
+    }
+})
