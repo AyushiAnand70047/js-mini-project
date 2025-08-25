a38714ebbad78371a71197bceacf57ac768412dc
AyushiAnand70047 <ayushianand088@gmail.com>
2025-08-25 11:46:27 +0530

    digital clock timer updated to handle multiple timer

--- Digital Clock/script.js
+++ Digital Clock/script.js
@@ -5,24 +5,31 @@ function showTime() {
     document.getElementById('clock').innerText = time;
 }
 
-setInterval(showTime,1000);
+setInterval(showTime, 1000);
 
 const startTime = document.getElementById('startTime');
 const timer = document.getElementById('timer');
 
 startTime.addEventListener('click', () => {
     let time = document.getElementById
-    ('time').value;
-    if(time != '' && time > 0) {
+        ('time').value;
+    if (time != '' && time > 0) {
+        timer.style.display = "block";
+        let p = document.createElement('p');
+        timer.appendChild(p);
+        document.getElementById('time').value = '';
         let interval = setInterval(() => {
-            if(time <= 0) {
+            if (time <= 0) {
                 clearInterval(interval)
-                timer.innerText = 'Time is up!';
+                p.innerText = 'Time is up!';
+                setTimeout(() => {
+                    p.remove();
+                }, 2000);
             }
-            else{
-                timer.innerText = time;
+            else {
+                p.innerText = time;
                 time--;
             }
-        },1000)
+        }, 1000)
     }
 })
