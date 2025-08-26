e789fec86ea508f144bda023a643a35fb4cd7ab6
AyushiAnand70047 <ayushianand088@gmail.com>
2025-08-26 08:59:26 +0530

    QR code generator created

---
+++ Qr Code Generator/script.js
@@ -0,0 +1,12 @@
+let imgBox = document.getElementById('img-box');
+let qrImage = document.getElementById('qr-image');
+
+function generateQR(){
+    let qrText = document.getElementById('qr-text');
+    if(qrText.value.length > 0) {
+        qrImage.src = "https://api.qrserver.com/v1/create-qr-code/?size=150x150&data=" + qrText.value;
+    } else {
+        alert("Enter text or url to generate qr");
+        qrImage.src = "";
+    }
+}
